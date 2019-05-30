cd9665afc645bc0f277906162d1fcbd1a5ef3139
Vladimir Kuznetsov <mistakster@gmail.com>
2019-05-30 17:50:45 +0500

    added production build for the document title

---
+++ examples/document-title/build/precache-manifest.fc6c4cd8c183646865b39b1e7f3a74ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef90f0b189885a50d8252337a7f4f34",
+    "url": "/uwdc-side-effects/examples/document-title/build/index.html"
+  },
+  {
+    "revision": "178e3b2a2f4af3ead461",
+    "url": "/uwdc-side-effects/examples/document-title/build/static/css/main.6248796c.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/uwdc-side-effects/examples/document-title/build/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "178e3b2a2f4af3ead461",
+    "url": "/uwdc-side-effects/examples/document-title/build/static/js/main.62825be8.chunk.js"
+  },
+  {
+    "revision": "a171d1b97691042658e4",
+    "url": "/uwdc-side-effects/examples/document-title/build/static/js/runtime~main.b0f46ff0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uwdc-side-effects/examples/document-title/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
