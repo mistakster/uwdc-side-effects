d7d936b5023d367c7f11f7229decc20566348aaa
Vladimir Kuznetsov <mistakster@gmail.com>
2019-05-30 17:05:26 +0500

    added production build for the city map

---
+++ examples/city-map/build/precache-manifest.246cef18a445bf4794959bed43a6df43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b21b7ce4c37a0b15ca7db008e77d1c",
+    "url": "/uwdc-side-effects/examples/city-map/build/index.html"
+  },
+  {
+    "revision": "c62d84fdfdf5c1f85858",
+    "url": "/uwdc-side-effects/examples/city-map/build/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "c1053af184e9b92f65f9",
+    "url": "/uwdc-side-effects/examples/city-map/build/static/css/main.b6f1234f.chunk.css"
+  },
+  {
+    "revision": "c62d84fdfdf5c1f85858",
+    "url": "/uwdc-side-effects/examples/city-map/build/static/js/2.3c40df58.chunk.js"
+  },
+  {
+    "revision": "c1053af184e9b92f65f9",
+    "url": "/uwdc-side-effects/examples/city-map/build/static/js/main.55ceb5fb.chunk.js"
+  },
+  {
+    "revision": "416d59bda3594a3d8abf",
+    "url": "/uwdc-side-effects/examples/city-map/build/static/js/runtime~main.35b97586.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uwdc-side-effects/examples/city-map/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
