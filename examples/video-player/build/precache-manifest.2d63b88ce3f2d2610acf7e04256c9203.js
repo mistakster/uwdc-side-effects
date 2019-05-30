65b4e1e7e8014168e228e2ebd07be10f1d0ba656
Vladimir Kuznetsov <mistakster@gmail.com>
2019-05-30 17:28:35 +0500

    added production build for the video player

---
+++ examples/video-player/build/precache-manifest.2d63b88ce3f2d2610acf7e04256c9203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eeeefb8d9110ae98d2f7c0327cfa760",
+    "url": "/uwdc-side-effects/examples/video-player/build/index.html"
+  },
+  {
+    "revision": "9f9d802d2a58bb3e5cba",
+    "url": "/uwdc-side-effects/examples/video-player/build/static/css/2.e1395683.chunk.css"
+  },
+  {
+    "revision": "6eb701b3adf93e9f2122",
+    "url": "/uwdc-side-effects/examples/video-player/build/static/css/main.6248796c.chunk.css"
+  },
+  {
+    "revision": "9f9d802d2a58bb3e5cba",
+    "url": "/uwdc-side-effects/examples/video-player/build/static/js/2.80d8aa1b.chunk.js"
+  },
+  {
+    "revision": "6eb701b3adf93e9f2122",
+    "url": "/uwdc-side-effects/examples/video-player/build/static/js/main.2ec07525.chunk.js"
+  },
+  {
+    "revision": "1d0c31d521fe3a108aef",
+    "url": "/uwdc-side-effects/examples/video-player/build/static/js/runtime~main.2374042f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uwdc-side-effects/examples/video-player/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
